b9cce967185ddc1f46283f7f6309d111426fb7ad
tedajax <ted.ajax@gmail.com>
2024-05-04 10:23:33 -0700

    delete some unused files that were causing confusion

--- src/common/config_editor.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "config.h"
-
-void config_editor_initialize(void);
-void config_editor_shutdown(void);
-void config_editor_set_config(h_config config_handle);
-void config_editor_show_window();
