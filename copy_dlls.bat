d68bb8c3c534268f6ddfff01941a104e882bc01d
tedajax <ted.ajax@gmail.com>
2025-07-19 08:41:43 -0700

    remove external sdl3 libs for windows since they are gotten from vcpkg now, update copy_dlls.bat to pull correct dlls into CWD

--- copy_dlls.bat
+++ copy_dlls.bat
@@ -1,9 +1,9 @@
 @echo off
 
 if "%1"=="debug" (
-	set "BINPATH=lib\Win64\Debug\"
+	set "BINPATH=vcpkg_installed\x64-windows\debug\bin\"
 ) else (
-	set "BINPATH=lib\Win64\Release\"
+	set "BINPATH=vcpkg_installed\x64-windows\bin\"
 )
 
 del *.dll
